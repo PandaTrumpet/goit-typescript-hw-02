1af46eb6bb5c58a8679e40fac8cbc80e61f9857f
PandaTrumpet <dsavontrumpet@gmail.com>
2024-06-01 20:37:27 +0300

    update

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,14 +1,24 @@
-import { FallingLines } from "react-loader-spinner";
+import { MutatingDots } from "react-loader-spinner";
 import css from "./Loader.module.css";
-export default function Loader() {
+
+interface AppProps {
+  isLoading: boolean;
+}
+const Loader: React.FC<AppProps> = ({ isLoading }) => {
   return (
     <div className={css.loader}>
-      <FallingLines
-        color=" rgb(102, 102, 240)"
+      <MutatingDots
+        visible={isLoading}
+        height="100"
         width="100"
-        visible={true}
-        ariaLabel="falling-circles-loading"
+        color="#4fa94d"
+        secondaryColor="#4fa94d"
+        radius="12.5"
+        ariaLabel="mutating-dots-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
       />
     </div>
   );
-}
+};
+export default Loader;
